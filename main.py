59c9731f0dc4f80fcd85add9c131e6c79b517ab4
Mathew Ross <matjross@gmail.com>
2025-04-06 12:38:40 -0600

    updated constants, and main, added player

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
   print("Starting Asteroids!")
   print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
   pygame.init()
   clock = pygame.time.Clock()
+  player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
   dt = 0
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   loop_state = True
@@ -15,8 +17,10 @@ def main():
         return
     
     screen.fill("black")
+    player.draw(screen)
     pygame.display.flip()
     
+
     # Limiting framerate to 60FPS
     dt = clock.tick(60)/1000
 
