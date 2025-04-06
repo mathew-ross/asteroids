61a5c55d08462dd9c49967f6193429be874f3300
Mathew Ross <matjross@gmail.com>
2025-04-06 12:12:16 -0600

    Updated program to limit game to 60 FPS, added .gitignore

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ def main():
   print("Starting Asteroids!")
   print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
   pygame.init()
+  clock = pygame.time.Clock()
+  dt = 0
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   loop_state = True
   while loop_state == True:
-    screen.fill("black")
-    pygame.display.flip()
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+    
+    screen.fill("black")
+    pygame.display.flip()
+    
+    # Limiting framerate to 60FPS
+    dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
   main()
